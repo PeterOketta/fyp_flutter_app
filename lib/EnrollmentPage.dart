1a2fb7d435101e4c8ba1f99983c22850721ec357
PeterOketta <97467356+PeterOketta@users.noreply.github.com>
2024-03-15 16:19:36 +0300

    fixed model not found

--- lib/EnrollmentPage.dart
+++ lib/EnrollmentPage.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'Enrollment_functions.dart';
 
-
 class EnrollmentScreen extends StatefulWidget {
   @override
   _EnrollmentScreenState createState() => _EnrollmentScreenState();
@@ -14,12 +13,17 @@ class _EnrollmentScreenState extends State<EnrollmentScreen> {
   BluetoothCharacteristic? _characteristic;
   final _enrollmentFunctions = EnrollmentFunctions();
 
+  @override
+  void initState() {
+    super.initState();
+    _enrollmentFunctions.loadModel();
+  }
+
   void _startEnrollment() async {
-    _enrollmentFunctions.enrollUser(80, _updateEnrollmentStatus);
+    await _enrollmentFunctions.enrollUser(80, _updateEnrollmentStatus);
     if (_isEnrolling == false) {
       Navigator.pushReplacementNamed(context, '/profile');
     }
-
   }
 
   void _updateEnrollmentStatus(bool isEnrolling, String status) {
@@ -39,35 +43,21 @@ class _EnrollmentScreenState extends State<EnrollmentScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            // Instructions
             Text(
               'Place the sensor on your garment as shown.',
               style: TextStyle(fontSize: 16.0),
             ),
-
             SizedBox(height: 20.0),
-
-            // Bluetooth Status (Update based on real status)
             Text(
               'Bluetooth: Connected',
               style: TextStyle(fontSize: 16.0),
             ),
-
             SizedBox(height: 20.0),
-
-            // Enrollment Button
             ElevatedButton(
-              onPressed: _isEnrolling
-                  ? null
-                  : () {
-                _startEnrollment();
-              },
+              onPressed: _isEnrolling ? null : _startEnrollment,
               child: Text(_isEnrolling ? 'Enrolling...' : 'Start Enrollment'),
             ),
-
             SizedBox(height: 20.0),
-
-            // Enrollment Status
             Text(_enrollmentStatus),
           ],
         ),
--- lib/Enrollment_functions.dart
+++ lib/Enrollment_functions.dart
@@ -1,103 +1,103 @@
 import 'dart:math';
+import 'package:flutter/services.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
 import '../utils/BluetoothUtils.dart';
 import '../utils/BluetoothManager.dart';
 import 'dart:io';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:path_provider/path_provider.dart';
+
 class EnrollmentFunctions {
   Interpreter? _interpreter;
   BluetoothUtils _bluetoothUtils = BluetoothUtils();
-  Future<void> loadModel(String modelPath) async {
-    _interpreter = await Interpreter.fromAsset(modelPath);
+
+  Future<void> loadModel() async {
+    try {
+      final modelData = await rootBundle.load('assets/attention_model.tflite');
+      _interpreter = await Interpreter.fromBuffer(modelData.buffer.asUint8List());
+      print('Model loaded successfully');
+    } catch (e) {
+      print('Error loading model: $e');
+    }
   }
+
   Future<List<List<int>>> segmentECGData(List<int> ecgData, int windowSize, int maxSegments) async {
-  List<List<int>> segments = [];
-  for (int i = 0; i < ecgData.length && segments.length < maxSegments; i += windowSize) {
-  int endIndex = i + windowSize;
-  if (endIndex > ecgData.length) endIndex = ecgData.length;
-  segments.add(ecgData.sublist(i, endIndex));
+    List<List<int>> segments = [];
+    for (int i = 0; i < ecgData.length && segments.length < maxSegments; i += windowSize) {
+      int endIndex = i + windowSize;
+      if (endIndex > ecgData.length) endIndex = ecgData.length;
+      segments.add(ecgData.sublist(i, endIndex));
+    }
+    print('Segments: $segments');
+    return segments;
   }
-  print('Segments: $segments');
-  return segments;
-  }
+
   Future<List> runInference(List<int> ecgSegment) async {
-    // Check if the interpreter is null, and load the model if needed
+    print('Running inference...');
     if (_interpreter == null) {
-      await loadModel('attention_model.tflite');
+      await loadModel();
     }
 
     if (_interpreter == null) {
+      print('Failed to load the model');
       throw Exception('Failed to load the model.');
     }
 
-    // 1. Prepare Input (assuming Float32List for input)
     List<double> inputData = ecgSegment.map((value) => value.toDouble()).toList();
     print('Input to model: $inputData');
     List<int> inputShape = _interpreter!.getInputTensor(0).shape;
     inputData = inputData.reshape(inputShape) as List<double>;
     var output = List.filled(150 * 128, 0).reshape([150, 128]);
     _interpreter!.run([inputData], output);
+    print('Inference completed');
     return output;
   }
-
 
   Future<void> enrollUser(int duration, Function setStateCallback) async {
     setStateCallback(true, 'Enrolling...');
     try {
-      // Retrieve Bluetooth characteristic from BluetoothManager
-      BluetoothCharacteristic? characteristic =
-      BluetoothManager.getCharacteristic();
+      print('Starting enrollment process...');
+      BluetoothCharacteristic? characteristic = BluetoothManager.getCharacteristic();
 
       if (characteristic == null) {
         throw Exception('Bluetooth characteristic not found.');
       }
 
-      // 2. Collect ECG Data
-      List<int> longECGSegment =
-      await _bluetoothUtils.collectDataFromCharacteristic(
-          Duration(seconds: duration), characteristic);
+      List<int> longECGSegment = await _bluetoothUtils.collectDataFromCharacteristic(Duration(seconds: duration), characteristic);
       print('Collected Data: $longECGSegment');
-      // 3. Segmentation
-      List<List<int>> segments = await segmentECGData(
-          longECGSegment, 200, 40);
 
-      // 4. Model Inference (Repeated for each segment)
+      List<List<int>> segments = await segmentECGData(longECGSegment, 200, 40);
+
       List<double> accumulatedContextVectors = [];
       int enrollmentSegmentsCount = 0;
       const enrollmentThreshold = 40;
 
-      while (enrollmentSegmentsCount < enrollmentThreshold &&
-          segments.isNotEmpty) {
+      while (enrollmentSegmentsCount < enrollmentThreshold && segments.isNotEmpty) {
         List<int> segment = segments.removeAt(0);
 
-        List<double> contextVector = await runInference(segment) as List<
-            double>;
+        List<double> contextVector = await runInference(segment) as List<double>;
 
         accumulatedContextVectors.addAll(contextVector);
 
         enrollmentSegmentsCount += 1;
 
-        // Progress Update
-        setStateCallback(true, 'Enrolling (${enrollmentSegmentsCount * 100 ~/
-            enrollmentThreshold}%');
+        setStateCallback(true, 'Enrolling (${enrollmentSegmentsCount * 100 ~/ enrollmentThreshold}%');
       }
 
-      // 5. iECG Calculation
       if (enrollmentSegmentsCount == enrollmentThreshold) {
-        List<double> iecg = calculateIECG(
-            accumulatedContextVectors.cast<List<double>>());
-        // File Storage
+        print('Calculating iECG...');
+        List<double> iecg = calculateIECG(accumulatedContextVectors.cast<List<double>>());
         try {
           final directory = await getApplicationDocumentsDirectory();
           final file = File('${directory.path}/iecg_data.txt');
           String iecgString = iecg.join(',');
           await file.writeAsString(iecgString);
-
+          print('iECG data stored successfully');
         } catch (error) {
           print('Error storing iECG: $error');
         }
       } else {
+        print('Enrollment failed: Not enough segments collected');
         throw Exception('Enrollment failed: Not enough segments collected');
       }
     } catch (error) {
@@ -106,35 +106,32 @@ class EnrollmentFunctions {
       rethrow;
     }
   }
+
   List<double> calculateIECG(List<List<double>> contextVectors) {
     if (contextVectors.isEmpty) {
       throw Exception('Cannot calculate iECG with no context vectors');
     }
 
-    // 1. Transpose for Easier Averaging (optional)
     List<List<double>> transposed = transpose(contextVectors);
 
-    // 2. Average across segments
     List<double> iecg = transposed.map((vector) => vector.reduce((a, b) => a + b) / vector.length).toList();
 
-    // 3. Normalization (Example: Z-score normalization)
     double mean = iecg.reduce((a, b) => a + b) / iecg.length;
     double stdDev = calculateStandardDeviation(iecg);
 
-    if (stdDev != 0.0) { // Avoid division by zero if all values are the same
+    if (stdDev != 0.0) {
       iecg = iecg.map((value) => (value - mean) / stdDev).toList();
     }
 
     return iecg;
   }
-  // Simple standard deviation calculation (you may find a more optimized version)
+
   double calculateStandardDeviation(List<double> data) {
     double mean = data.reduce((a, b) => a + b) / data.length;
     num sumOfSquares = data.map((value) => pow(value - mean, 2)).reduce((a, b) => a + b);
     return sqrt(sumOfSquares / data.length);
   }
 
-// Helper for transposing lists
   List<List<double>> transpose(List<List<double>> matrix) {
     List<List<double>> transposed = [];
     for (int i = 0; i < matrix[0].length; i++) {
@@ -142,5 +139,4 @@ class EnrollmentFunctions {
     }
     return transposed;
   }
-
 }
